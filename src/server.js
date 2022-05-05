ba6795f4d00f4dee0d8008f50aa9948cdcc2ca73
pushkarwagh <puwagh@bestpeers.com>
2022-05-05 16:44:19 +0530

    connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dot_env = require('dotenv');
 var cors = require('cors');
+const host = "0.0.0.0";
 
 require('./connection.js');
 const routes = require('./routes.js');
@@ -16,6 +17,6 @@ dot_env.config();
 const port = process.env.PORT || 8000 ;
 console.log("dote env ==>",process.env.PORT) 
 
-app.listen(port ,() => 
+app.listen(port, host, () => 
     console.log( `server listening on ${port}`)
 );
